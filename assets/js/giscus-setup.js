53246cf9f3360357544940e7a320a0c3c03e0f4c
OLLab-SYSU <wangjiah@mail.sysu.edu.cn>
2026-02-19 17:37:06 +0000

    Deploying to gh-pages from @ OLLab-SYSU/ollab-sysu.github.io@2f2016f84c00e502e4fd6093d132cfeaadc732d0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "L2Obook/l2obook.github.io",
+    "data-repo": "OLLab-SYSU/ollab-sysu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
