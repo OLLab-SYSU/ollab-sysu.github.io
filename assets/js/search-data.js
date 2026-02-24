d672e664019d5a7bd17ffc98c93746abc455a9bc
Chenhui7au <jason.lchwork@gmail.com>
2026-02-24 02:38:33 +0000

    Deploying to gh-pages from @ OLLab-SYSU/ollab-sysu.github.io@5072178a9f3506a7c8cfd897b5e7809e09caf546 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,20 +3,13 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-学习优化",
-    title: "学习优化",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-关于作者",
-          title: "关于作者",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/about/";
-          },
-        },{id: "post-a-post-with-plotly-js",
+  },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
